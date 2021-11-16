9c380701b396631cd807157457f962feacf9e9b9
devmaxwel <eng.maxwel.ochieng@gmail.com>
2021-11-17 01:29:48 +0300

    fourth Commit

--- src/Components/MainNavigation/MainNavigation.jsx
+++ src/Components/MainNavigation/MainNavigation.jsx
@@ -10,16 +10,18 @@ function MainNavigation() {
             <h1 className="logo">Fanaka Oasis Realtors</h1>
             <ul className={responsive ? "nav-links-mobile" : "nav-links"}
                 onClick={() => setIsResponsive(false)}>
+
                 <Link to='/' className="home"><li>Home</li></Link>
                 <Link to='/realtor-profile' className="about"><li>About</li></Link>
-                <Link to='/rentals' className="rentals"><li>rentals</li></Link>
+                <Link to='/rentals' className="rentals"><li>Rentals</li></Link>
               <div className="actions">
              <Link to='/contact'> <button>Contact Us</button></Link>
               </div>
             </ul>
 
-            <div className="mobile-menu-icon" onClick ={() => setIsResponsive(responsive)}>
-                {responsive ? <i class="fas fa-times"></i> : <i class="fas fa-bars"></i>}
+            <div className="mobile-menu-icon" onClick ={() => setIsResponsive(!responsive)}>
+                {responsive ? <i class="fas fa-times"></i> 
+                : <i class="fas fa-bars"></i>}
 
             </div>
 
