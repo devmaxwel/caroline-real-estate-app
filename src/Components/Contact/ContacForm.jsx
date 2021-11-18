665509ed6eb799911cca62226ce157b5cd8a8d9e
devmaxwel <eng.maxwel.ochieng@gmail.com>
2021-11-18 10:21:47 +0300

    envelope update

--- src/Components/Contact/ContacForm.jsx
+++ src/Components/Contact/ContacForm.jsx
@@ -22,7 +22,7 @@ import './Form.css';
                                         <p>+254 728-479-603</p>
                                     </div>
                                 </div>
-                                <div class="d-flex flex-row align-items-center mt-3"> <span class="icons"><i class="far fa-envelope"></i></span>
+                                <div class="d-flex flex-row align-items-center mt-3"> <span class="icons"><i class="fa fa-envelope"></i></span>
                                     <div class="address text-left"> <span>Address</span>
                                         <p>fanakaoasisluxurioushomes@gmail.com</p>
                                     </div>
